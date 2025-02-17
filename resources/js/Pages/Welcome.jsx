e7c8fd35a00079d58cd962f2bc8cb31ccaeca526
Ken <ashartey2@gmail.com>
2025-02-17 18:05:31 +0000

    project dependency installation and auth

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,46 @@
+import { Head, Link } from "@inertiajs/react";
+
+export default function Welcome({ auth, laravelVersion, phpVersion }) {
+    const handleImageError = () => {
+        document
+            .getElementById("screenshot-container")
+            ?.classList.add("!hidden");
+        document.getElementById("docs-card")?.classList.add("!row-span-1");
+        document
+            .getElementById("docs-card-content")
+            ?.classList.add("!flex-row");
+        document.getElementById("background")?.classList.add("!hidden");
+    };
+
+    return (
+        <>
+            <header className="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
+                <nav className="-mx-3 flex flex-1 justify-end">
+                    {auth.user ? (
+                        <Link
+                            href={route("dashboard")}
+                            className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] text-black"
+                        >
+                            BethPoll
+                        </Link>
+                    ) : (
+                        <>
+                            <Link
+                                href={route("login")}
+                                className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-black "
+                            >
+                                Log in
+                            </Link>
+                            <Link
+                                href={route("register")}
+                                className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
+                            >
+                                Register
+                            </Link>
+                        </>
+                    )}
+                </nav>
+            </header>
+        </>
+    );
+}
