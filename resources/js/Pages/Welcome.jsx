4445becebadacc2632852510120d3be882e31d43
Ken <ashartey2@gmail.com>
2025-03-30 12:45:51 +0000

    candidate image

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -34,6 +34,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                 className="rounded-md  px-3 py-2 bg-blue-500 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
                             >
                                 Dashboard
+                                {/* Hello */}
                             </Link>
                         </div>
                     ) : (
